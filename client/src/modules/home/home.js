05ba8a2643526d14f533484fe30bde201367d8f5
hoanganh hoanganh <anhvmh@gem-corp.jp>
2025-12-05 23:23:01 +0900

    add link

--- client/src/modules/home/home.js
+++ client/src/modules/home/home.js
@@ -138,7 +138,7 @@ function Home() {
                             {Array.from({ length: recordsPerPage }, (_, index) => {
                                 if (activeStep * recordsPerPage + index < totalTh) {
                                     return (<Grid item>
-                                        <Link to={'du-lieu-quy-hoach?id=' + ketquaQuyhoach[activeStep * recordsPerPage + index]?.danhgiaquyhoachView?.id} key={ketquaQuyhoach[activeStep * recordsPerPage + index]?.danhgiaquyhoachView?.id}>
+                                        <Link to={'du-lieu-quy-hoach?id=' + ketquaQuyhoach[activeStep * recordsPerPage + index]?.danhgiaquyhoach_view?.id} key={ketquaQuyhoach[activeStep * recordsPerPage + index]?.danhgiaquyhoach_view?.id}>
                                             <Box
                                                 sx={{
                                                     border: '1px solid #3A5BFF',
